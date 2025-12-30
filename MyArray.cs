547ff12bf23f87009077d483450bb38d1b76147a
Balan Andrei <abalan91@yahoo.com>
2025-12-30 12:14:44 +0200

    Basic implementation of an Array class

---
+++ MyArray.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresRefresher
+{
+    public class MyArray
+    {
+        private static int _length;
+        private static int[] _data;
+
+        public int GetLength () { return _length; }
+        public MyArray(int length) { 
+            _length = length;
+            _data = new int[length];
+        }
+
+        public int Get(int index)
+        {
+            return _data[index];
+        }
+
+        public int Push(int item)
+        {
+            if (_data.Length == _length)
+            {
+                int[] newData = new int[_length * 2];
+
+                for (int i = 0;i < _data.Length; i++)
+                {
+                    newData[i] = _data[i];
+                }
+                _data = newData;
+            }
+
+            _data[_length] = item;
+            _length ++;
+            return _length;
+        }
+
+        public int Pop()
+        {
+            int itemToDelete = _data[_length - 1];
+            _data[_length - 1] = 0;
+
+            return itemToDelete;
+        }
+
+        public int Delete(int index)
+        {
+            int itemToDelete = _data[index];
+            shitItems(index);
+
+            return itemToDelete;
+        }
+
+        private static void shitItems(int index)
+        {
+            for (int i = index; i < _length; i++)
+            {
+                _data[i] = i++;
+            }
+            _data[_length - 1] = 0;
+            _length --;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DataStructuresRefresher;
+
+MyArray array = new MyArray(3);
+Console.WriteLine(array.Get(2));
+Console.WriteLine(array.Get(1));
+Console.WriteLine(array.Get(0));
+
+array.Push(1);
+Console.WriteLine(array.Get(3));
+array.Pop();
+Console.WriteLine(array.Get(3));
+array.Delete(1);
+Console.WriteLine(array.GetLength());
