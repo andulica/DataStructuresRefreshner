e2476fd1a78f33f9b7244c8b4c03164558bf85b1
Balan Andrei <abalan91@yahoo.com>
2025-12-30 19:12:14 +0200

    MergeTwoSortedArrays solution

--- Program.cs
+++ Program.cs
@@ -1,13 +1,82 @@
 ﻿using DataStructuresRefresher;
 
-MyArray array = new MyArray(3);
-Console.WriteLine(array.Get(2));
-Console.WriteLine(array.Get(1));
-Console.WriteLine(array.Get(0));
+//MyArray array = new MyArray(3);
+//Console.WriteLine(array.Get(2));
+//Console.WriteLine(array.Get(1));
+//Console.WriteLine(array.Get(0));
 
-array.Push(1);
-Console.WriteLine(array.Get(3));
-array.Pop();
-Console.WriteLine(array.Get(3));
-array.Delete(1);
-Console.WriteLine(array.GetLength());
+//array.Push(1);
+//Console.WriteLine(array.Get(3));
+//array.Pop();
+//Console.WriteLine(array.Get(3));
+//array.Delete(1);
+//Console.WriteLine(array.GetLength());
+
+//static void ReverseString (string stringToReverse)
+//{
+//    if (string.IsNullOrEmpty(stringToReverse))
+//    {
+//        return;
+//    }
+
+//    char [] reversedString = new char [stringToReverse.Length];
+
+//    int temp = 0;
+//    for (int i = stringToReverse.Length - 1; i >= 0; i--) 
+//    {
+//        reversedString [temp] = stringToReverse [i];
+//        temp++;
+//    }
+
+//    Console.WriteLine (reversedString);
+//}
+
+//ReverseString("Andrei");
+
+static void MergeTwoSortedArrays ( int[] firstArray, int[] secondArray)
+{
+    int arrayWithMaxChars = firstArray.Length + secondArray.Length;
+
+    
+
+    int[] mergedArray = new int[arrayWithMaxChars];
+
+    int j = 0;
+    int k = 0;
+    int index = 0;
+
+    while (j < firstArray.Length && k < secondArray.Length)
+    {
+        if (firstArray[j] <= secondArray[k])
+        {
+            mergedArray[index] = firstArray[j];
+            j++;
+            index++;
+        } else
+        {
+            mergedArray[index] = secondArray[k];
+            k++;
+            index++;
+        }
+    }
+    while (j < firstArray.Length)
+    {
+        mergedArray[index] = firstArray[j];
+        j++;
+        index++;
+    }
+
+    while (k < secondArray.Length)
+    {
+        mergedArray[index] = secondArray[k];
+        k++;
+        index++;
+    }
+
+    foreach (int i in mergedArray)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+MergeTwoSortedArrays([0, 3, 4, 31], [4, 6, 30]);
